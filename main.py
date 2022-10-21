b83c95724ca4ea64a793db686bfa09fe5285d811
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2022-10-21 13:49:39 +0200

    improve code

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import argparse, os
 import shutil
 from tqdm import tqdm
 
+def get_cid(path):
+    return os.popen(f'ipfs add -n -Q {path}', 'r').read()
+
+def my_full_path(string):
+    script_dir = os.path.dirname(__file__)
+    return  os.path.normpath(os.path.join(script_dir, string))
+
+def rename_and_copy(file,fromDir, outputDir):
+    extension_file = os.path.splitext(file)[1]
+
+    input_path = os.path.join(fromDir, file)
+    hash = get_cid(input_path)
+
+    output_filename = hash.strip() + extension_file
+    output_path = os.path.join(outputDir, output_filename)
+    
+    shutil.copyfile(input_path, output_path)
+
 def main():
-    def get_cid(path):
-        return os.popen(f'ipfs add -n -Q {path}', 'r').read()
-
-    def my_full_path(string):
-        script_dir = os.path.dirname(__file__)
-        return  os.path.normpath(os.path.join(script_dir, string))
-
-    def rename_and_copy(file):
-        extension_file = os.path.splitext(file)[1]
-
-        input_path = os.path.join(args.fromDir, file)
-        hash = get_cid(input_path)
-
-        output_filename = hash.strip() + extension_file
-        output_path = os.path.join(args.outputDir, output_filename)
-        
-        shutil.copyfile(input_path, output_path)
-
     parser = argparse.ArgumentParser(description='Copy file in a directory and rename them with their IPFS CIDv0.')
     parser.add_argument('--fromDir', required=True, type=my_full_path)
     parser.add_argument('--outputDir', required=True, type=my_full_path)
@@ -35,7 +35,7 @@ def main():
     files = os.listdir(args.fromDir)
 
     for file in tqdm(files):        
-        rename_and_copy(file)
+        rename_and_copy(file, args.fromDir, args.outputDir)
 
     print('Done.')
 
